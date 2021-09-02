4815118aec95ec9eeda6b248b496c71080501426
Mahir <mahirfaisalv@gmail.com>
2021-09-03 00:13:37 +0600

    commit

---
+++ rules_conventions/pep8.py
@@ -0,0 +1,31 @@
+# pip install black python  formatter col 79
+# indentation 4 spaces
+# naming variable snake case word_word int my_number no pascale HelloWorld or camelcase Helloworls
+# constant variable HELLO_WORLD COLOR=() Top of program below import
+# naming convention one word lower case
+# functions def hello_world() snake case
+# classes pascal case class BaseClass: or class Base:
+# exception class Exception:  class XException:
+# first parameter we name self and instance
+# class Test:
+# def test(self):
+# pass
+# @classmethod
+# def cls_method(cls):
+# pass
+# separate classes and functions with two blank ,if nested 1 blank
+# import module in two different lines
+# from os import path,stat in single line
+# do not import everything import * from os
+# no white space between spam(1) or spam(1,)
+# correct
+# i = i + 1
+# submitted += 1
+# incorrect
+# x= x *2 -1
+# hypo2 = x * x + y* y
+# comment example COLOR = (0, 255, 0)  # This is correct
+# while checking prefix or suffix of String
+# if foo.startswith("bar")/endswith("bar"):
+# pass
+# not use if foo[:3] == "bar"
